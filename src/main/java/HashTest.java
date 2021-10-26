3db04bc06c500f678c848eac0c511d29fd77049a
afonse03 <amanda.m.fonseca03@gmail.com>
2021-10-25 22:25:06 -0600

    finished passwords exercise

---
+++ src/main/java/HashTest.java
@@ -0,0 +1,8 @@
+import org.mindrot.jbcrypt.BCrypt;
+
+public class HashTest {
+    public static void main(String[] args) {
+        String salt = BCrypt.gensalt();
+        System.out.println(salt);
+    }
+}
